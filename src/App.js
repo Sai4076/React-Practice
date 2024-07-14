fb3a8053c778561c9dfcd4de5c9e8f7dd61eee80
Sai4076 <munvspradeep1996@gmail.com>
2024-07-14 18:31:46 +0530

    Added code for addtask
    Added code for addtask

--- src/App.js
+++ src/App.js
@@ -2,16 +2,21 @@ import { Header } from "./components/Header";
 import { Footer } from "./components/Footer";
 import { TaskList } from "./components/TaskList";
 import { AddTask } from "./components/AddTask";
+import { useState } from "react";
 
 import './App.css';
 
 function App() {
 
+  const [tasks, setTasks] = useState([]);
+  
   return (
     <div className="App">
       <Header />
-      <AddTask />
-      <TaskList />
+      <main>
+        <AddTask tasks={tasks} setTasks={setTasks}/>
+        <TaskList tasks={tasks} setTasks={setTasks}/>
+      </main>
       <Footer />
     </div>
   );
